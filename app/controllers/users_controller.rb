77b93cf71a457a3f37c22bae2a4fa141a3c92121
Lena Plaksina <lena.plaksina@gmail.com>
2018-11-22 10:51:50 +1300

    Remove email logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,6 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     user.save!
 
-    MailerWorker.perform_async("UserMailer", "welcome_email", { "user" => user.id })
-
     redirect_to(new_session_path)
   end
 
--- app/jobs/mailer_worker.rb
+++
@@ -1,8 +0,0 @@
-class MailerWorker
-  include Sidekiq::Worker
-
-  def perform(mail_class, mail_name, params)
-    mailer = mail_class.constantize
-    mailer.with(params).send(mail_name.to_sym).deliver_now
-  end
-end
