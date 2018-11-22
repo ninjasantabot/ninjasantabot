f16f53b5a8caa9370066d28be5e966c0e10813a9
CDeighton <cullumdeighton@gmail.com>
2018-11-22 14:47:19 +1300

    Plugged messaging into target generation

--- app/services/create_user_pairings.rb
+++ app/services/create_user_pairings.rb
@@ -4,12 +4,15 @@ class CreateUserPairings
   def initialize(game, users)
     @game = game
     @users = users
+    @bot = SantaBot.new
   end
 
   def call
     Pairing.transaction do
       users.shuffle.cycle.each_cons(2).take(users.size).each do |ninja, target|
         @game.pairings.create!(ninja: ninja, target: target)
+
+        @bot.message_user(user: ninja.email, target: target.email, message: :target_assignment)
       end
     end
   end
