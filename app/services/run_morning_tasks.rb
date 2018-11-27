9384bdbe1237e613fade2839e47587dc09513604
Eilis Hamilton <eilis@fluxfederation.com>
2018-11-27 21:04:04 +1300

    Move sending of daily notifications for active games into a service

--- app/services/run_morning_tasks.rb
+++ app/services/run_morning_tasks.rb
@@ -1,59 +1,12 @@
 class RunMorningTasks
   def call
     puts "In RunMorningTasks"
-    send_notifications
+    send_daily_notifications_for_active_games
   end
 
   private
 
-  def send_notifications
-    puts "in send notifications"
-    game.in_progress.each do |game|
-      puts "sending notifications for game #{game.id}"
-      day_index = game.current_day_index
-      partitions = partition_users(game)
-
-      partitions[:ninjas].each do |user|
-        puts "- sending ninjas notifications"
-        send_notification(user, message_name("ninjas", day_index))
-      end
-
-      partitions[:targets].each do |user|
-        puts "- sending targets notifications"
-        send_notification(user, message_name("targets", day_index))
-      end
-
-      partitions[:ninja_and_targets].each do |user|
-        puts "- sending ninjas and targets notifications"
-        send_notification(user, message_name("ninjas_and_targets", day_index))
-      end
-    end
-  end
-
-  def partition_users(game)
-    surviving_ninjas = game.pairings.active.map(&:ninja)
-    pondering_targets = game.pairings.active.map(&:target)
-
-    {
-      targets: pondering_targets - surviving_ninjas,
-      ninjas: surviving_ninjas - pondering_targets,
-      ninja_and_targets: surviving_ninjas & pondering_targets,
-    }
-  end
-
-  def send_notification(user, message)
-    bot.message_user(user: user.uid, message: message)
-  end
-
-  def message_name(type, day_index)
-    day_text = {
-      0 => 'day_one',
-      1 => 'day_two',
-      2 => 'day_three',
-      3 => 'day_four',
-      4 => 'day_five',
-    }[day_index]
-
-    "ninja_mail.notifications.#{type}.#{day_text}"
+  def send_daily_notifications_for_active_games
+    SendDailyNotificationsForActiveGames.new.call
   end
 end
