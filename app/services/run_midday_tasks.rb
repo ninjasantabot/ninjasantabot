0376cc498bfc2ed097294ab82282f1bd9e7c0e51
Eilis Hamilton <eilis@fluxfederation.com>
2018-11-27 21:13:19 +1300

    Add logging for starting games

--- app/services/run_midday_tasks.rb
+++ app/services/run_midday_tasks.rb
@@ -13,6 +13,7 @@ class RunMiddayTasks
 
   def start_games
     games.where(signup_end_date: Date.today).each do |game|
+      puts "creating pairing for game #{game.id}"
       CreateUserPairings.new(game).call
     end
   end
