82985750665b5f59e180025d8aa5cf0383640172
Michael Fowler <mkrfowler@gmail.com>
2018-11-29 18:26:56 +1300

    redirect after joining a game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -38,6 +38,7 @@ class GamesController < ApplicationController
   def join
     @game = Game.find(params[:id])
     @game.user_games.create!(user: current_user)
+    redirect_to @game
   end
 
   private
