e4bac6d60f4727f3702eab4d96bae5dda4c520e8
Michael Fowler <mkrfowler@gmail.com>
2018-11-30 17:11:34 +1300

    Handle the case where some users have entered clues for all the days

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,5 +1,6 @@
 module GameHelper
-  def not_all_days_have_clues?(game)
-    game.days.order("days.created_at DESC").includes(:clues).where(clues: { id: nil } ).last.date <= game.game_end_date
+  def not_all_days_have_clues?(game, user)
+    clued_days = game.days.joins(:clues).where(clues: { user_id: user.id })
+    clued_days.size < game.days.size
   end
 end
