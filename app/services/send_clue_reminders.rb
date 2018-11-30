741a7fc9050fb55446a7a5ccebb7ebd42fa8fb67
Michael Fowler <mkrfowler@gmail.com>
2018-11-30 15:11:06 +1300

    Avoid nil deferences in `SendClueReminders`

--- app/services/send_clue_reminders.rb
+++ app/services/send_clue_reminders.rb
@@ -2,6 +2,7 @@ class SendClueReminders
   def call
     puts "in SendClueReminders"
     game_groupings = Game.waiting_for_clues.group_by(&:days_until_start)
+    game_groupings.default_value = []
 
     game_groupings[3].each do |game|
       send_reminder_to_all_users(game, "ninja_mail.reminder.two_days")
