e89bed6e25b758084c5846893f59550360440598
Michael Fowler <mkrfowler@gmail.com>
2018-12-03 18:58:18 +1300

    Update schema.rb, because I'm forgetful

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_232211) do
+ActiveRecord::Schema.define(version: 2018_12_03_052452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2018_11_29_232211) do
     t.bigint "game_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "guessed_at"
     t.boolean "guessed", default: false, null: false
     t.index ["game_id"], name: "index_pairings_on_game_id"
     t.index ["ninja_id"], name: "index_pairings_on_ninja_id"
