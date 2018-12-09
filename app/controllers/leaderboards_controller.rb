3267bb6d8f50c7675678d269e9223ebdb4583e2d
Michael Fowler <mkrfowler@gmail.com>
2018-12-10 12:15:30 +1300

    Compensatory correction for inverting `active?`

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -3,7 +3,7 @@ class LeaderboardsController < ApplicationController
 
   def show
     @game = Game.find(params[:game_id])
-    @successful, @pending = @game.pairings.preload(:target, :ninja).partition(&:active?)
+    @pending, @successful = @game.pairings.preload(:target, :ninja).partition(&:active?)
     @successful.sort_by!(&:guessed_at)
     @pending.sort_by! { |pair| pair.target.name }
   end
